9199468fb1e183582040e59760a63fd9ecd5a1d9
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2023-03-06 19:22:25 +0900

    [Add] PostData type 추가

---
+++ src/types/post.ts
@@ -0,0 +1,10 @@
+export interface PostData {
+    title: string;
+    summary: string;
+    slug: string;
+    category: string;
+    series: string;
+    tag: string[];
+    thumbnail: string;
+    date: string;
+}
