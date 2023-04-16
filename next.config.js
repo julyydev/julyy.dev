ef707a66ddf01b9a8c9cc44027295d5bf234cc76
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2023-04-16 17:22:54 +0900

    [Chore] next/image remotePatterns 추가

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
         styledComponents: true,
     },
     images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'drive.google.com',
+                port: '',
+                pathname: '/uc',
+            },
+            {
+                protocol: 'https',
+                hostname: '**.googleusercontent.com',
+            },
+        ],
         domains: ['drive.google.com'],
         path: '/_next/image',
         formats: ['image/avif', 'image/webp'],
