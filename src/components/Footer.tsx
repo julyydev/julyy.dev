a8aa6d8c1c6cb27dfbf6f2af5bf2c1b107f3ccea
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2023-04-16 16:26:37 +0900

    [Design] Footer 가운데 정렬 및 구분 선 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,27 @@ import { themedPalette } from '@/styles/themes';
 import styled from 'styled-components';
 
 const Footer = () => {
-    return <CopyRight>© 2023 Julyy, All Rights Reserved.</CopyRight>;
+    return (
+        <Wrapper>
+            <Divider />
+            <CopyRight>© 2023 Julyy, All Rights Reserved.</CopyRight>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const Divider = styled.hr`
+    margin-top: 500px;
+    width: 80%;
+    height: 1px;
+    background-color: black;
+    border: 0;
+`;
 
 const CopyRight = styled.div`
     color: ${themedPalette.text1};
