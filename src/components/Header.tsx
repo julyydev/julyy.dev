e4784490f9f09b1345c855a383b3734d5dbb372f
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2023-05-03 17:57:06 +0900

    [Feat] Header 현재 메뉴 표시 기능 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,48 @@
 import Link from 'next/link';
-import React from 'react';
 import styled from 'styled-components';
 import ThemeToggleButton from './ThemeToggleButton';
 import { themedPalette } from '@/styles/themes';
 import SearchButton from './SearchButton';
+import { Pacifico } from '@next/font/google';
+import { notoSansKR } from '@/styles/fonts/notoSansKR';
+import useActiveMenu from '@/hooks/useActiveMenu';
 
 const Header = () => {
+    const { activeMenu, setActiveMenu } = useActiveMenu();
+
     return (
         <HeaderWrapper>
             <Title>
-                <LogoLink href={'/'}>
-                    julyy.dev<Beta>βeta</Beta>
+                <LogoLink href={'/'} onClick={() => setActiveMenu('')}>
+                    Julyy.dev<Beta>βeta</Beta>
                 </LogoLink>
             </Title>
             <Navigation>
                 <SearchButton />
                 <ThemeToggleButton />
-                <NavigationItem>
-                    <MenuLink href="/blog">Blog</MenuLink>
+                <NavigationItem isActive={activeMenu === 'blog'}>
+                    <MenuLink
+                        href="/blog"
+                        onClick={() => setActiveMenu('blog')}
+                    >
+                        Blog
+                    </MenuLink>
                 </NavigationItem>
-                <NavigationItem>
-                    <MenuLink href="/projects">Projects</MenuLink>
+                <NavigationItem isActive={activeMenu === 'projects'}>
+                    <MenuLink
+                        href="/projects"
+                        onClick={() => setActiveMenu('projects')}
+                    >
+                        Projects
+                    </MenuLink>
                 </NavigationItem>
-                <NavigationItem>
-                    <MenuLink href="/about">About</MenuLink>
+                <NavigationItem isActive={activeMenu === 'about'}>
+                    <MenuLink
+                        href="/about"
+                        onClick={() => setActiveMenu('about')}
+                    >
+                        About
+                    </MenuLink>
                 </NavigationItem>
             </Navigation>
         </HeaderWrapper>
@@ -32,15 +51,23 @@ const Header = () => {
 
 export default Header;
 
+const font = Pacifico({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'fallback',
+});
+
 const HeaderWrapper = styled.header`
     background-color: ${themedPalette.bg_page1};
     color: ${themedPalette.text1};
     display: flex;
     justify-content: space-between;
     padding: 20px;
+    font-family: ${notoSansKR.style.fontFamily};
 `;
 
 const Title = styled.h1`
+    font-family: ${font.style.fontFamily};
     font-size: 30px;
     margin: 0;
 `;
@@ -63,13 +90,22 @@ const Navigation = styled.nav`
     display: flex;
 `;
 
-const NavigationItem = styled.div`
-    color: ${themedPalette.text1};
-    font-size: 18px;
+interface NavigationItemProps {
+    isActive: boolean;
+}
+
+const NavigationItem = styled.div<NavigationItemProps>`
+    /* color: ${themedPalette.text1}; */
+    color: ${props => {
+        if (props.isActive) return '#9980fa';
+        else return 'gray';
+    }};
+    font-size: 16px;
     margin-left: 20px;
     text-decoration: none;
 
     &:hover {
-        color: #cccccc;
+        color: black;
+        text-decoration: underline;
     }
 `;
