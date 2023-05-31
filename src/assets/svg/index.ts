ab6f497f03f2a83e4b66fc50ff92478a1ca9210e
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2023-05-31 12:15:25 +0900

    [Add] remove icon 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -10,6 +10,7 @@ export { default as LeftAngleIcon } from './icon_left_angle.svg';
 export { default as RightAngleIcon } from './icon_right_angle.svg';
 export { default as DoubleLeftAngleIcon } from './icon_double_left_angle.svg';
 export { default as DoubleRightAngleIcon } from './icon_double_right_angle.svg';
+export { default as RemoveIcon } from './icon_remove.svg';
 
 export { default as CategoryCppIcon } from './category/category_cpp.svg';
 export { default as CategoryJavaScriptIcon } from './category/category_javascript.svg';
