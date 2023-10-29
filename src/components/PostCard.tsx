910da4b5609d68592b9b52f752940f8c0d38e204
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2023-10-30 02:25:13 +0900

    [Fix] PostCard 수정

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,6 +6,7 @@ import CategoryLabel from './CategoryLabel';
 import { themedPalette } from '@/styles/themes';
 import { CalendarIcon, TagIcon } from '@/assets/svg';
 import TagLabel from '@/components/TagLabel';
+import Link from 'next/link';
 
 interface Props {
     postData: PostData;
@@ -15,42 +16,44 @@ const PostCard = (props: Props) => {
     const { postData } = props;
 
     return (
-        <CardContainer>
-            <Thumbnail>
-                <Image
-                    src={postData.thumbnail}
-                    alt={postData.title}
-                    placeholder="blur"
-                    blurDataURL={postData.thumbnail}
-                    sizes="(max-width: 768px) 100vw,
+        <StyledLink href={`/blog/${postData.slug}`}>
+            <CardContainer>
+                <Thumbnail>
+                    <Image
+                        src={postData.thumbnail}
+                        alt={postData.title}
+                        placeholder="blur"
+                        blurDataURL={postData.thumbnail}
+                        sizes="(max-width: 768px) 100vw,
                       (max-width: 1200px) 50vw,
                       33vw"
-                    width={100}
-                    height={100}
-                    style={{ width: '100%', height: '100%' }}
-                />
-            </Thumbnail>
-            <CardContent>
-                <CardContentTopWrapper>
-                    <CategoryLabel value={postData.category} />
-                    <DateWrapper>
-                        <CalendarIcon
-                            width={12}
-                            height={12}
-                            fill={themedPalette.text3}
-                        />
-                        <Date>{postData.date}</Date>
-                    </DateWrapper>
-                </CardContentTopWrapper>
-                <Title>{postData.title}</Title>
-                <Summary>{postData.summary}</Summary>
-                <TagWrapper>
-                    {postData.tag.map(tag => (
-                        <TagLabel key={tag} value={tag}></TagLabel>
-                    ))}
-                </TagWrapper>
-            </CardContent>
-        </CardContainer>
+                        width={100}
+                        height={100}
+                        style={{ width: '100%', height: '100%' }}
+                    />
+                </Thumbnail>
+                <CardContent>
+                    <CardContentTopWrapper>
+                        <CategoryLabel value={postData.category} />
+                        <DateWrapper>
+                            <CalendarIcon
+                                width={12}
+                                height={12}
+                                fill={themedPalette.text3}
+                            />
+                            <Date>{postData.date}</Date>
+                        </DateWrapper>
+                    </CardContentTopWrapper>
+                    <Title>{postData.title}</Title>
+                    <Summary>{postData.summary}</Summary>
+                    <TagWrapper>
+                        {postData.tag.map(tag => (
+                            <TagLabel key={tag} value={tag}></TagLabel>
+                        ))}
+                    </TagWrapper>
+                </CardContent>
+            </CardContainer>
+        </StyledLink>
     );
 };
 
@@ -75,13 +78,13 @@ const CardContainer = styled.div`
 
 const Thumbnail = styled.div`
     width: 100%;
-    height: 50%;
+    height: 180px;
     object-fit: cover;
 `;
 
 const CardContent = styled.div`
     width: 100%;
-    height: 50%;
+    height: 200px;
     display: flex;
     justify-content: center;
     flex-direction: column;
@@ -102,6 +105,7 @@ const Title = styled.div`
     font-family: ${nanumGothic.bold.style.fontFamily};
     color: ${themedPalette.text1};
     margin-top: 16px;
+    height: 40px;
 `;
 
 const Summary = styled.div`
@@ -115,13 +119,16 @@ const Summary = styled.div`
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 2;
     overflow: hidden;
+    //background-color: skyblue;
 `;
 
 const TagWrapper = styled.div`
     width: calc(100% - 32px);
+    height: 24px;
     display: flex;
     align-items: center;
     flex-wrap: wrap;
+    overflow: hidden;
 `;
 
 const DateWrapper = styled.div`
@@ -135,4 +142,20 @@ const Date = styled.div`
     margin-left: 5px;
     font-size: 12px;
     color: ${themedPalette.text3};
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    width: 320px;
+    height: 380px;
+
+    @media (max-width: 992px) {
+        width: 100%;
+        height: 100%;
+    }
+
+    @media (max-width: 768px) {
+        width: 100%;
+        height: 100%;
+    }
 `;
